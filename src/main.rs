7d948ccb875603849838461e9b013144ad84cc4e
Shark-Firewall <Kumarrajnish363.rk@gmail.com>
2025-01-02 21:05:18 +0530

    Coded basic look & Feel

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
 use std::env;
+use std::io;
+use std::io::stdin;
 
 
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    println!("To do list using rust!");
+enum Operations {
+    List,
+    Add,
+    Edit,
+    Completed
 }
 
+fn perform_operation(operation: Operations){
+    match operation {
+        Operations::List => println!("You have selected List Operation!!!"),
+        Operations::Add => println!("You have selected Add Operation!!!"),
+        Operations::Edit => println!("You have selected Edit Operation!!!"),
+        Operations::Completed => println!("You have selected the Completed Operations!!!")  
+    }
+}
+
+fn main() {
+    println!("Welcome to to Rust Todo List!");
+    println!("\n------------ Select below Operations ------------------");
+    println!("1 - Show todo list");
+    println!("2 - Add items to todo list");
+    println!("3 - Edit any item");
+    println!("4 - Mark a item completed");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read value");
+    let mut user:Operations;
+    if input == "1" {
+        user = Operations::List;
+        perform_operation(user);
+    }
+
+}
 
 // Command
-// 1 - Add
-// 2 - Remove
-// 3 - Edit
+// 1 - List  -- List all the item 
+// 2 - Add
+// 3 - Completed
+// 4 - Edit
 
 // Argument pattern
-// ---------------------- Add Item ----------------
+// cargo run show
 // cargo run <Add> <Value>
+// cargo run <Completed> <Index_Number>
+// cargo run <Edit> <Index_Number> <New value>
